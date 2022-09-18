ea6cddfc79668f0c5868d568390f739f6f5620b9
ahykho <ikram.senguen@live.de>
2022-09-18 03:46:19 +0200

    sghdf

--- app/src/main/java/com/nullhuman/bmichecker/NutriActivity.java
+++ app/src/main/java/com/nullhuman/bmichecker/NutriActivity.java
@@ -3,12 +3,19 @@ package com.nullhuman.bmichecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import java.util.Objects;
 
 public class NutriActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // trying to show the fuckn back button...
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        // trying to show the fuckn back button...
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutri);
     }
@@ -17,9 +24,15 @@ public class NutriActivity extends AppCompatActivity {
 
     // experimental back button.. not working
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
     public void onBackPressed() {
         super.onBackPressed();
         this.onPause();
     }
 }
-
--- app/src/main/java/com/nullhuman/bmichecker/SportActivity.java
+++ app/src/main/java/com/nullhuman/bmichecker/SportActivity.java
@@ -3,12 +3,19 @@ package com.nullhuman.bmichecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import java.util.Objects;
 
 public class SportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // trying to show the fuckn back button...
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        // trying to show the fuckn back button...
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport);
     }
@@ -16,6 +23,13 @@ public class SportActivity extends AppCompatActivity {
     }
 
     // experimental back button.. not working
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
