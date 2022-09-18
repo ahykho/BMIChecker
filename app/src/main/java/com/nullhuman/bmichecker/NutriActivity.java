e5d081f14c3a9f453cd15c4e32faf638ac8c274f
ahykho <ikram.senguen@live.de>
2022-09-18 21:02:47 +0200

    private to public, no reason to hide

--- app/src/main/java/com/nullhuman/bmichecker/NutriActivity.java
+++ app/src/main/java/com/nullhuman/bmichecker/NutriActivity.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class NutriActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutri);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
--- app/src/main/java/com/nullhuman/bmichecker/SportActivity.java
+++ app/src/main/java/com/nullhuman/bmichecker/SportActivity.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class SportActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
